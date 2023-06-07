35471745d9c59b2e7a1fc25326dd7b61a8ae63a3
Hsin Ron <48174229+wang-xinrong@users.noreply.github.com>
2023-06-07 14:53:13 +0800

    Fixed Some Merge Conflicts and Sprint 2 Builds are Now Ready

--- Odyssey/Assets/Scripts/Enemy/AnimationNames.cs
+++ Odyssey/Assets/Scripts/Enemy/AnimationNames.cs
@@ -11,4 +11,9 @@ public class AnimationNames
     public static string VillagerGirlHurt = "VillagerGirlHurt";
     public static string VillagerGirlDeath = "VillagerGirlDeath";
 
+    public static string ZbjIdle = "Idle";
+    public static string ZbjWalk = "Walk";
+    public static string ZbjHurt = "Hurt";
+    public static string ZbjDeath = "Death";
+
 }

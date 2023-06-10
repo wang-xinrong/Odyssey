f81485c1e45dc86ce9a37a6f6ab5bedbf16634c4
gavingoh99 <gavingoh99@gmail.com>
2023-06-10 22:16:42 +0800

    misc scene updates

--- Odyssey/Assets/Scripts/SpecialAttack/MKSpecialAttack.cs
+++ Odyssey/Assets/Scripts/SpecialAttack/MKSpecialAttack.cs
@@ -11,12 +11,12 @@ public class MKSpecialAttack : MonoBehaviour
         GameObject Shockwave = this.transform.Find("Shockwave").gameObject;
         Shockwave.SetActive(true);
         Collider2D[] colliders = Physics2D.OverlapCircleAll(_playerController.transform.position ,5f, AoE);
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.5f);
         foreach (Collider2D collider in colliders)
         {
             collider.GetComponent<Damageable>().OnHurt(30, Vector2.zero);
         }
         yield return new WaitForSeconds(1.0f);
-        Shockwave.SetActive(true);
+        Shockwave.SetActive(false);
     }
 }

c15f7d83c734f2ca1f384770115f1ad2acb3b042
Hsin Ron <48174229+wang-xinrong@users.noreply.github.com>
2023-06-15 10:02:00 +0800

    Completed Extending SpecialEffects to AIPath Component Carrying Enemies
    Enemies Carrying AIPath Component Would Need to Add AISpecialEffect Component to be Affected by Special Effects

--- Odyssey/Assets/Scripts/SpecialEffect/AISpecialEffect.cs
+++ Odyssey/Assets/Scripts/SpecialEffect/AISpecialEffect.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
-public class AISpecialEffect : MonoBehaviour
+public class AISpecialEffect : MonoBehaviour, EnemyUnderSpecialEffect
 {
     private AIPath _aIPath;
+    private float _originalMovementSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         _aIPath = GetComponent<AIPath>();
+    }
+
+    public void SlowedDown(float fractionOfOriginalSpeed, float duration)
+    {
+        _originalMovementSpeed = _aIPath.maxSpeed;
+        _aIPath.maxSpeed = _aIPath.maxSpeed * fractionOfOriginalSpeed;
+        Invoke("ResetMovementSpeed", duration);
+    }
+
+    private void ResetMovementSpeed()
+    {
+        _aIPath.maxSpeed = _originalMovementSpeed;
     }
 }

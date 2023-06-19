ec70117cdc579d2084af61a7301c3ccd32caa122
Hsin Ron <48174229+wang-xinrong@users.noreply.github.com>
2023-06-19 22:58:10 +0800

    Automated Testing Seventh Attempt

--- Odyssey/Assets/Tests/Editor/DirectionTests.cs
+++ Odyssey/Assets/Tests/Editor/DirectionTests.cs
@@ -11,11 +11,11 @@ public class DirectionTests
     public void DirectionTestsSimplePasses()
     {
 
-        Assert.AreEqual(Vector2.down, Directions.RightFiveDirections[0]);
+        Assert.AreEqual(Vector2.up, Directions.RightFiveDirections[0]);
         // Use the Assert class to test conditions
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // A UnityTest behaves like a coroutine in Play de. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
     public IEnumerator DirectionTestsWithEnumeratorPasses()

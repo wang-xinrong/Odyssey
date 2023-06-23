51402f68939fc8e1f3a376bb287ec23f3ca184f0
gavingoh99 <gavingoh99@gmail.com>
2023-06-23 17:26:14 +0800

    added new animations for second zbj weapon

---
+++ Odyssey/Assets/Scripts/Weapon/Weapons/ZBJ/GoldTrident/GoldTridentAttack0.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldTridentAttack0 : WeaponAttack
+{
+    public GoldTridentAttack0()
+    {
+        animationName = "GoldAttack0";
+        damage = 5;
+    }
+}
---
+++ Odyssey/Assets/Scripts/Weapon/Weapons/ZBJ/GoldTrident/GoldTridentAttack2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldTridentAttack2 : WeaponAttack
+{
+    public GoldTridentAttack2()
+    {
+        animationName = "GoldAttack2";
+        damage = 5;
+    }
+}

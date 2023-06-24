26dbaa8441cfc3bb4b108819a286ef4831ac9c85
gavingoh99 <gavingoh99@gmail.com>
2023-06-24 15:25:19 +0800

    added guard clause for death

--- Odyssey/Assets/Scripts/Player/PlayerController.cs
+++ Odyssey/Assets/Scripts/Player/PlayerController.cs
@@ -143,6 +143,10 @@ public class PlayerController : MonoBehaviour, PlayerUnderSpecialEffect
 
     public void OnAttack(InputAction.CallbackContext context)
     {
+        if (_currentState == State.Death)
+        {
+            return;
+        }
         if (context.started)
         {
             Attack();

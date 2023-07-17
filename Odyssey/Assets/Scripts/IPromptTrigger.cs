69ed12b93e2914b7baca744bd179f6d1d2246ccc
Hsin Ron <48174229+wang-xinrong@users.noreply.github.com>
2023-07-17 20:43:32 +0800

    Implemented IKeyPrompt

---
+++ Odyssey/Assets/Scripts/IPromptTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IPromptTrigger : KeyPromptTrigger
+{
+    protected override bool OnCondition()
+    {
+        return InventoryManager.Instance.AnyItemInUsageRow();
+    }
+
+    protected override bool OnTerminateCondition()
+    {
+        return OnCondition();
+    }
+}

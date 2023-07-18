e4e48114772b2495ace6b3a238a65a4542423a10
gavingoh99 <gavingoh99@gmail.com>
2023-07-18 15:03:19 +0800

    misc

--- Odyssey/Assets/Scripts/SpecialAttack/Shockwave.cs
+++ Odyssey/Assets/Scripts/SpecialAttack/Shockwave.cs
@@ -6,9 +6,9 @@ public class Shockwave : MonoBehaviour
 {
     public Animator Animator;
     // Start is called before the first frame update
-    void Start()
-    {
-        Animator = GetComponent<Animator>();
-        Animator.Play("Shockwave");
-    }
+    // void Start()
+    // {
+    //     Animator = GetComponent<Animator>();
+    //     Animator.Play("Shockwave");
+    // }
 }

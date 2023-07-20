32a5bf1659a70be83df207d89f5a82f129aa5a22
gavingoh99 <gavingoh99@gmail.com>
2023-07-21 01:16:59 +0800

    added animations for gold staff mk

---
+++ Odyssey/Assets/Scripts/Weapon/Weapons/MK/GoldStaff/GoldStaffAttack0.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldStaffAttack0 : WeaponAttack
+{
+    public GoldStaffAttack0()
+    {
+        animationName = "GoldAttack0";
+        damage = 20;
+    }
+}
---
+++ Odyssey/Assets/Scripts/Weapon/Weapons/MK/GoldStaff/GoldStaffAttack1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldStaffAttack1 : WeaponAttack
+{
+    public GoldStaffAttack1()
+    {
+        animationName = "GoldAttack1";
+        damage = 20;
+    }
+}

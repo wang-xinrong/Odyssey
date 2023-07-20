e7d849905c4b28803a80babaa7204cc44d9638a0
gavingoh99 <gavingoh99@gmail.com>
2023-07-20 23:57:48 +0800

    added animations for silver staff mk

---
+++ Odyssey/Assets/Scripts/Weapon/Weapons/MK/SilverStaff/SilverStaffAttack0.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SilverStaffAttack0 : WeaponAttack
+{
+    public SilverStaffAttack0()
+    {
+        animationName = "SilverAttack0";
+        damage = 20;
+    }
+}

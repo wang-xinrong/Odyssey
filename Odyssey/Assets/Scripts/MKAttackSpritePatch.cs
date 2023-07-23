8ad284f06cabd4815ae11cffb0128cc018f103bc
Hsin Ron <48174229+wang-xinrong@users.noreply.github.com>
2023-07-23 16:44:59 +0800

    Debugged MK's Hitbox Deactivation Issue

--- Odyssey/Assets/Scripts/MKAttackSpritePatch.cs
+++ Odyssey/Assets/Scripts/MKAttackSpritePatch.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class MKAttackSpritePatch : MonoBehaviour
 {
     public SpriteRenderer[] spriteRenderers;
+    public Collider2D[] colliders;
 
     public void DeactivateSprites()
     {
         for (int i = 0; i < spriteRenderers.Length; i++)
         {
             spriteRenderers[i].enabled = false;
+            colliders[i].enabled = false;
         }
     }
 }

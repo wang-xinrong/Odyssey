bf86f343fde4d1d190f5961edc68840cb5a483d8
Hsin Ron <48174229+wang-xinrong@users.noreply.github.com>
2023-08-09 13:26:59 +0800

    Included Boss Melee Attack into Stats System

--- Odyssey/Assets/Scripts/BossStatistics.cs
+++ Odyssey/Assets/Scripts/BossStatistics.cs
@@ -7,11 +7,15 @@ public class BossStatistics : MonoBehaviour
 {
     public AIPath AIPath;
     public BossStageManager BossStageManager;
+    public Attack BossMeleeAttack;
 
     // Start is called before the first frame update
     void Start()
     {
         AIPath.maxSpeed = StatsManager.Instance.GetBossMS_AD_SI(
             BossStageManager.BossCodeName, 0);
+
+        BossMeleeAttack.AttackDamage = StatsManager.Instance
+            .GetBossMeleeDamage(BossStageManager.BossCodeName);
     }
 }
